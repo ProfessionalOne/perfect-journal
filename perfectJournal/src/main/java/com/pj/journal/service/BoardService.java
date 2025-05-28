d521d38f10d614a08083611110ecb15ff744d7d6
Mino <minho00123@gmail.com>
2025-05-28 20:06:41 +0900

    feat: create exception handling

--- perfectJournal/src/main/java/com/pj/journal/service/BoardService.java
+++ perfectJournal/src/main/java/com/pj/journal/service/BoardService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.ui.Model;
 
@@ -12,48 +13,43 @@ import com.pj.journal.model.board.BoardVo;
 
 @Service
 public class BoardService {
-	
+	@Autowired
 	SqlSessionFactory sqlSessionFactory;
 
-	public BoardService(SqlSessionFactory sqlSessionFactory) {
-		super();
-		this.sqlSessionFactory = sqlSessionFactory;
-	}
-
 	public void getBoardList(Model model, int page) {
-        int pageSize = 10;
-        int offset = (page - 1) * pageSize;
+		int pageSize = 10;
+		int offset = (page - 1) * pageSize;
 
-        try (SqlSession session = sqlSessionFactory.openSession()) {
-            BoardDao boardDao = session.getMapper(BoardDao.class);
+		try (SqlSession session = sqlSessionFactory.openSession()) {
+			BoardDao boardDao = session.getMapper(BoardDao.class);
 
-            List<BoardVo> boardList = boardDao.selectByPage(offset, pageSize);
-            int totalCount = boardDao.getTotalCount();
-            int totalPages = (int) Math.ceil((double) totalCount / pageSize);
-            int beginPage = pageSize * ((page - 1) / pageSize) + 1;
-            int endPage = beginPage + (pageSize - 1);
-            if (endPage > totalPages) {
-            	endPage = totalPages;
-            }
+			List<BoardVo> boardList = boardDao.selectByPage(offset, pageSize);
+			int totalCount = boardDao.getTotalCount();
+			int totalPages = (int) Math.ceil((double) totalCount / pageSize);
+			int beginPage = pageSize * ((page - 1) / pageSize) + 1;
+			int endPage = beginPage + (pageSize - 1);
+			if (endPage > totalPages) {
+				endPage = totalPages;
+			}
 
-            model.addAttribute("boardList", boardList);
-            model.addAttribute("currentPage", page);
-            model.addAttribute("beginPage", beginPage);
-            model.addAttribute("endPage", endPage);
-            model.addAttribute("totalPages", totalPages);
-        }
-    }
-	public BoardVo getBoardList(int postId) {
-		 try (SqlSession session = sqlSessionFactory.openSession()) {
-	            return session.getMapper(BoardDao.class).selectOneBoard(postId);
-		 }
+			model.addAttribute("boardList", boardList);
+			model.addAttribute("currentPage", page);
+			model.addAttribute("beginPage", beginPage);
+			model.addAttribute("endPage", endPage);
+			model.addAttribute("totalPages", totalPages);
+		}
 	}
-	
+
+	public BoardVo getBoardList(int postId) {
+		try (SqlSession session = sqlSessionFactory.openSession()) {
+			return session.getMapper(BoardDao.class).selectOneBoard(postId);
+		}
+	}
+
 	public void addBoardList(BoardVo bean) {
 		try (SqlSession session = sqlSessionFactory.openSession();) {
 			session.getMapper(BoardDao.class).insertOneBoard(bean);
 		}
 	}
-	
 
 }
